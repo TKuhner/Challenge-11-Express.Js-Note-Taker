3c1d2d83b0513172047ab60c49cf8b721889f538
TKuhner <trevorkuhner29@gmail.com>
2023-06-15 20:07:53 -0500

    initial commit, added provided work.

---
+++ server.js
@@ -0,0 +1,33 @@
+// import required modules and packages
+
+// setup express.js server
+
+// define routes
+    
+    // get route for home page
+       // return index.html
+
+    // get route for notes page
+         // return notes.html
+
+    // get route to retrieve all saved notes
+        // read db.json file
+        // return all saved notes as json
+
+    // post route to add new saved notes
+        // read db.json file
+        // parse the request body to get the new note data
+        // assign a unique id
+        // push this new note to the array of saved notes
+        // write the updated array of notes the db.json file
+        // return the new note to the client
+
+    // delete route to remove a saved note based on id
+        // read db.json file
+        // get the id of the note to be able to remove
+        // find the note with the corresponding id in the array
+        // remove the note from the array
+        // write the updated array of notes with the deleted missing
+
+// use the helper folder for your uuid and utils
+
