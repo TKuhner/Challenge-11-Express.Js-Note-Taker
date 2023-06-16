e7aea49f131f96fadecd1950d5e3d83d82e39540
TKuhner <trevorkuhner29@gmail.com>
2023-06-15 20:08:54 -0500

    added necessary set up to server.js

--- server.js
+++ server.js
@@ -31,3 +31,16 @@
 
 // use the helper folder for your uuid and utils
 
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
